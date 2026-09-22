432a188fbf12fdfd13603b76c7147de05a6ef6c3
taifanura <m.taifanura@gmail.com>
2026-09-22 14:28:29 +0600

    Add files via upload

---
+++ Day 2/exm2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    pair<string, int> student = {"Ariful", 101};
+ 
+    cout << "Name: " << student.first << ", Roll: " << student.second << "\n";
+  
+return 0; 
+
+}
