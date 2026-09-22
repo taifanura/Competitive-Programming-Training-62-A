c44c0d71fe8f4a0bd2569f4efbd8a8bf851804da
taifanura <m.taifanura@gmail.com>
2026-09-22 14:42:23 +0600

    Add files via upload

---
+++ Day 2/str2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1 = "Competitive";
+    string s2 = "Programming";
+    
+    
+    string s = s1 + " " + s2; 
+    
+    cout << "Result: " << s << "\n";
+    
+    return 0;
+}
